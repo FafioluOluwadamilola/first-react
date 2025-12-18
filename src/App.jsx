60ffb2a9c91ace3d961c13f4873477e38e2c034a
Fafiolu Oluwadamilola <info4dammyfaffy2022@gmail.com>
2025-12-18 17:16:13 +0100

    done with practical

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+
+const Card = ({title}) => {
+  const [hasLiked, setHasLiked] = useState(false);
+  return (
+    <div className="card">
+      <h2>{title}</h2>
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? "❤️" : "💗"}
+      </button>
+    </div>
+  )
+}
+
+
+
+const App = () => {
+
+
+
+  return (
+    <div className="card-container">
+      
+  
+      <Card title="Star Wars" rating={5}  isCool={true} />
+      <Card title="Wednesday" />
+      <Card title="The Lion King" />
+    </div>
+  )
+}
+
+export default App
