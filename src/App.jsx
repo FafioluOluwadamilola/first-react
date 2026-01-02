5e727e6cbb8f12ec79291556ba12f030be060f3b
Fafiolu Oluwadamilola <info4dammyfaffy2022@gmail.com>
2026-01-02 15:19:24 +0100

    Done with Everything

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Search from './components/search'
 import Spinner from './components/spinner'
 import MovieCard from './components/MovieCard';
 import { useDebounce } from 'react-use';
-import { updateSearchCount } from './appwrite';
+import { getTrendingMovies, updateSearchCount } from './appwrite';
 
 const API_BASE_URL = "https://api.themoviedb.org/3"
 
@@ -22,6 +22,7 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [movieList, setMovieList] = useState([]);
+  const [trendingMovies, setTrendingMovies] = useState([])
   const [isLoading, setIsLoading] = useState(false)
   const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('')
 
@@ -53,7 +54,9 @@ const App = () => {
 
       setMovieList(data.results) || [];
 
-      updateSearchCount();
+      if(query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0]);
+      }
 
     } catch (error) {
       console.log(`Error Fetching Movies: ${error}`)
@@ -63,10 +66,24 @@ const App = () => {
     }
   }
 
+  const loadTrendingMovies = async () => {
+    try {
+      const movies =  await getTrendingMovies()
+
+      setTrendingMovies(movies)
+
+    } catch (error) {
+      console.log(`Error fetching trending movies ${error}`)
+    }
+  }
+
   useEffect(() => {
     fetchMovies(debouncedSearchTerm);
   }, [debouncedSearchTerm]);
 
+  useEffect(() => {
+    loadTrendingMovies();
+  }, []);
 
   return (
     <main>
@@ -85,8 +102,23 @@ const App = () => {
 
         </header>
 
+        {trendingMovies.length > 0 && (
+        <section className='trending'>
+          <h2>Trending Movies</h2>
+
+          <ul>
+            {trendingMovies.map((movie, index) => (
+              <li key={movie.$id}>
+                <p>{index + 1}</p>
+                <img src={movie.poster_url} alt={movie.title} />
+              </li>
+            ))}
+          </ul>
+        </section>
+        )}
+
         <section className='all-movies'>
-          <h2 className='mt-[40px]'>All Movies</h2>
+          <h2>All Movies</h2>
           {isLoading ? (
             <Spinner />
           ) : errorMessage ? (
--- src/appwrite.js
+++ src/appwrite.js
@@ -1,4 +1,4 @@
-import { Client, Databases, Query } from "appwrite";
+import { Client, Databases, ID, Query } from "appwrite";
 
 
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
@@ -13,21 +13,40 @@ const database = new Databases(client);
 
 export const updateSearchCount = async (searchTerm, movie) => {
    //1 use Apppwrite SDK to check if search term exist in the database
-    try {
-        const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, queries: [
-            Query.equal("searchTerm", searchTerm)
-        ])
+   try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+        Query.equal("searchTerm", searchTerm)
+    ]);
 
-        if(result.documents.length > 0) {
-            const doc = result.documents[0];
-
-            await database.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id, )
+    //2 if exist, update the count field by 1
+    if(result.documents.length > 0) {
+        const doc = result.documents[0];
+        await database.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id , {
+            count: doc.count + 1
+        })
+    //3 if not exist, create a new record with count field set to 1
+        }else{
+            await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
+                searchTerm,
+                count: 1,
+                movie_id: movie.id,
+                poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+            })
         }
 
+   } catch (error) {
+    console.log(error)
+   }
+}
+
+export const getTrendingMovies = async () => {
+    try {
+        const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+            Query.limit(5),
+            Query.orderDesc("count")
+        ])
+        return result.documents;
     } catch (error) {
         console.log(error)
     }
-
-   //2 if exist, update the count field by 1
-   //3 if not exist, create a new record with count field set to 1
 }
